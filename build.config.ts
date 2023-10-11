7dcaf23118799f4c96823ef0920095fdc069b170
userquin <userquin@gmail.com>
2023-10-11 21:12:07 +0200

    chore: use antfu packages for eslint and changelog

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,9 @@
-import { defineBuildConfig } from "unbuild";
+import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true,
   },
-  entries: ["src/index"],
-});
+  entries: ['src/index'],
+})
