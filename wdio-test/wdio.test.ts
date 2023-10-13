4675b8e4902de3ca991cc6f7bb651bf8cce8764a
userquin <userquin@gmail.com>
2023-10-13 17:32:24 +0200

    chore: add missing wdio detection test stuff

---
+++ wdio-test/wdio.test.ts
@@ -0,0 +1,31 @@
+import { browser, expect } from '@wdio/globals'
+import { platform } from 'std-env'
+import { detect } from 'detect-browser-es'
+
+const browserName = browser.capabilities.browserName
+
+describe('WebdriverIO detection', () => {
+  it('WebdriverIO Detection', () => {
+    expect(detect()?.type).toBe('webdriverio')
+  })
+  if (browserName === 'chrome') {
+    it('Chrome', () => {
+      expect(detect()?.name).toBe('chrome')
+    })
+  }
+  if (browserName === 'edge') {
+    it('Edge', () => {
+      expect(detect()?.name).toBe('edge-chromium')
+    })
+  }
+  if (browserName === 'firefox') {
+    it('FireFox', () => {
+      expect(detect()?.name).toBe('firefox')
+    })
+  }
+  if (platform === 'darwin' && browserName === 'safari') {
+    it('Safari', () => {
+      expect(detect()?.name).toBe('safari')
+    })
+  }
+})
