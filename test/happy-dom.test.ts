50586066a5298113f62a35257408df208f972512
userquin <userquin@gmail.com>
2024-09-03 02:40:57 +0200

    chore: update jsdom/happy-dom inline snapshots

--- test/happy-dom.test.ts
+++ test/happy-dom.test.ts
@@ -10,6 +10,6 @@ describe('happy-dom', () => {
     const browser = detect()
     expect(browser).toBeDefined()
     expect(browser?.type).toBe('happy-dom')
-    expect(browser?.version).toMatchInlineSnapshot(`"0.0.0"`)
+    expect(browser?.version).toMatchInlineSnapshot('"0.0.0"')
   })
 })
